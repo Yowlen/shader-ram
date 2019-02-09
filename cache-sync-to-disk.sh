45efdd359a62e4ec5b3231f7b54233e056ed893c
Yowlen <17078692+Yowlen@users.noreply.github.com>
2019-02-08 23:28:25 -0500

    Forgot some files
    Not sure why these weren't included in the commits earlier, but here they are

---
+++ cache-sync-to-disk.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define some variables
+u=$(who | awk '{print $1}')
+shader_config=/home/$u/.config/shader-ram
+
+# Start by wrapping everything into a big ol' error catcher
+# in case the RAM disk wasn't mounted.
+if [ ! -d '/mnt/shader-ram' ]
+then
+    rm -f /home/$u/steam-shader-ram.log
+    echo 'Error: Shader RAM directory does not exist.' >> /home/$u/steam-shader-ram.log
+    echo 'Please ensure your /etc/fstab settings are correct.' >> /home/$u/steam-shader-ram.log
+else
+    # Back up the nVidia cache
+    # Comment out these lines if you don't use an nVidia GPU.
+    rsync -ar /mnt/shader-ram/.nv/ /home/$u/.nv/
+    chown -Rf $u /home/$u/.nv/
+
+    # Now for the Steam libraries.
+    # If you don't use Steam, you'll wanna either comment out
+    # or remove everything from here on except for the final
+    # "fi" and blank lines at the end.
+
+    # Check to make sure our Steam config file exists, and if not,
+    # create it.
+    if [ ! -f $shader_config/steamlibraries.config ]
+    then
+        /opt/shader-ram/cache-config.sh
+    fi
+
+    # Now back up each Steam library
+    for i in `cat $shader_config/steamlibraries.config`; do
+        cd $i
+        d=$(echo $i | cut -d '/' -f2-3 | tr '/' '-')
+        rsync -ar /mnt/shader-ram/steam-shader-$d/ ./shadercachelink/
+    done
+fi
---
+++ cache-sync-to-ram.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Define some variables
+u=$(who | awk '{print $1}')
+shader_config=/home/$u/.config/shader-ram
+
+    # First, let's check to see if the RAM disk is created,
+    # and if not, make it.
+    if [ ! -f '/mnt/shader-ram/.ramdisk' ]
+        mkdir /mnt/shader-ram
+        mount -t tmpfs -o size=2G tmpfs /mnt/shader-ram
+        touch /mnt/shader-ram/.ramdisk
+    fi
+
+    # Next, let's set up the config files, including Steam library detection.
+    /opt/shader-ram/cache-config.sh
+
+    # Now to take each Steam library and...
+    for i in `cat $shader_config/steamlibraries.config`; do
+        cd $i
+
+        # 1. Check to see if it's already linked, and if not, make a backup
+        #    on the hard drive (for first-time setup of new libraries)
+        if [ ! -L ./shadercache ]
+        then
+            rsync -ar ./shadercache/ ./shadercachelink/
+            rm -r ./shadercache
+            ln -s ./shadercachelink ./shadercache
+        fi
+
+        # 2. Sync the backup to the RAM disk, then change the link
+        #    to the RAM disk
+        d=$(echo $i | cut -d '/' -f2-3 | tr '/' '-')
+        rsync -ar ./shadercachelink/ /mnt/shader-ram/steam-shader-$d/
+        rm ./shadercache
+        ln -s /mnt/shader-ram/steam-shader-$d ./shadercache
+    done
+
+    # Populate the global nVidia shader cache
+    # Comment out these lines if you don't use an nVidia GPU.
+    rsync -ar /home/$u/.nv/ /mnt/shader-ram/.nv/
+    export __GL_SHADER_DISK_CACHE_PATH='/mnt/shader-ram/.nv'
+    export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
